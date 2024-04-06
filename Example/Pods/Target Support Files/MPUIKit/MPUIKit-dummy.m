b0124636d56900d55ac1a9cd3b572a12e1f74f00
Marian Polek <marian.polek@gmail.com>
2024-04-06 23:13:05 +0200

    init commit
    init commit

---
+++ Example/Pods/Target Support Files/MPUIKit/MPUIKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MPUIKit : NSObject
+@end
+@implementation PodsDummy_MPUIKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-MPUIKit/Pods-MPUIKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MPUIKit : NSObject
+@end
+@implementation PodsDummy_Pods_MPUIKit
+@end
